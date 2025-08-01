99a397b2aae7b76c0a7d9935917241af12cf5880
dhiman10 <dhimandas10.sec@gmail.com>
2025-08-01 14:40:07 +0200

    Add first

---
+++ TestCICD/TestCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCICDApp.swift
+//  TestCICD
+//
+//  Created by Dhiman Das on 01.08.25.
+//
+
+import SwiftUI
+
+@main
+struct TestCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
